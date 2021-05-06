dacf96370d2d20ff5314d086320627d96a402f5d
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2021-05-06 08:14:48 +0300

    exception init

---
+++ src/main/java/com/chocobo/composite/exception/CompositeException.java
@@ -0,0 +1,20 @@
+package com.chocobo.composite.exception;
+
+public class CompositeException extends Exception {
+
+    public CompositeException() {
+        super();
+    }
+
+    public CompositeException(String message) {
+        super(message);
+    }
+
+    public CompositeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CompositeException(Throwable cause) {
+        super(cause);
+    }
+}
